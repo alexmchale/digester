91165fef6bb6a10d14049955734465853311a46f
Alex McHale <alex@anticlever.com>
2014-08-15 06:09:18 -0500

    fix user session sign-in

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,2 +1,12 @@
 class UserSessionsController < InheritedResourcesController
+
+  def create
+    super do |success, failure|
+      success.html do
+        self.current_user = @user_session.user
+        redirect_to articles_path
+      end
+    end
+  end
+
 end
